9f1fe782ac4f5ed30a72fdbaa07b0a1a58a18432
Mohd Kamil <mohkamil.info@gmail.com>
2025-08-17 14:19:35 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,25 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  // Disable Next.js hot reload, handled by nodemon for recompilation
+  reactStrictMode: false,
+  webpack: (config, { dev }) => {
+    if (dev) {
+      // Disable webpack hot module replacement
+      config.watchOptions = {
+        ignored: ['**/*'], // Ignore all file changes
+      };
+    }
+    return config;
+  },
+  eslint: {
+    // Ignore ESLint errors during build
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
